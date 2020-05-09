d1d43a9c6f431c278a0556042d456985f207acdc
1995kkxl <mrc2017@foxmail.com>
2020-05-09 18:22:13 +0800

    添加10万条测试数据到数据库 sql语句

--- base_python/05_mysql操作/05_索引_插入10w条数据.py
+++ base_python/05_mysql操作/05_索引_插入10w条数据.py
@@ -1,5 +1,9 @@
 from pymysql import connect
 
+
+# 创建索引
+# create index title_index on test_index(title(10));
+
 def main():
     #创建Connection连接
     conn = connect(host="127.0.0.1",database="jing_dong",user="root",password="19951023chen",charset="utf8")
