bce73b1ce9bd491f12b05bde88576fbbb1151ec1
1995kkxl <mrc2017@foxmail.com>
2020-05-28 20:28:36 +0800

    bs

---
+++ base_python/13_jQuery/ajax/server.js
@@ -0,0 +1,28 @@
+//====================>html.js
+//引入http，模块
+var http=require('http');
+//引入url模块 解析请求的url
+var url=require('url')
+//引入IO文件模块 操作文件
+var fs=require('fs')
+
+//创建server 并指定服务器请求响应函数
+http.createServer(function(req,res){
+  var pathname=url.parse(req.url).pathname;
+  if(pathname=='/'){
+    //请求 index.html 返回请求状态为200
+    var data = fs.readFileSync('./index.html').toString();//读取文件内容并转换为字符串
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(data);
+  }else{
+    console.log('console.log-->404 fail');
+    //不是请求 index.html 返回请求状态为404
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end('404 fail');
+  }
+})
+//指定服务器监听的端口
+.listen(8888);
+console.log('服务器开启在：http://localhost:8888/');
+
+//=======================>index.html
